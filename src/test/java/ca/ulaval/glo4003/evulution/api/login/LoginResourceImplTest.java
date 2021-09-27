7950de15afe4e453e919a5b97b155898349143d4
VinceBro <vbreault99@gmail.com>
2021-09-27 11:29:16 -0400

    added validator tests

--- src/test/java/ca/ulaval/glo4003/evulution/api/login/LoginResourceImplTest.java
+++ src/test/java/ca/ulaval/glo4003/evulution/api/login/LoginResourceImplTest.java
@@ -1,10 +1,13 @@
 package ca.ulaval.glo4003.evulution.api.login;
 
 import ca.ulaval.glo4003.evulution.api.assemblers.HTTPExceptionResponseAssembler;
+import ca.ulaval.glo4003.evulution.api.exceptions.BadInputParameterException;
 import ca.ulaval.glo4003.evulution.api.login.dto.LoginDto;
+import ca.ulaval.glo4003.evulution.api.mappers.HTTPExceptionMapper;
 import ca.ulaval.glo4003.evulution.api.validators.ConstraintsValidator;
 import ca.ulaval.glo4003.evulution.domain.login.exception.NoAccountFoundException;
 import ca.ulaval.glo4003.evulution.service.login.LoginService;
+import jakarta.ws.rs.core.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,15 +15,17 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @ExtendWith(MockitoExtension.class)
 public class LoginResourceImplTest {
+    private static final Integer BAD_INPUT_PARAMETERS_ERROR_CODE = 400;
+    private static final String BAD_INPUT_PARAMETERS_ERROR_MESSAGE = "bad input parameter";
     private LoginResource loginResource;
+    private HTTPExceptionResponseAssembler httpExceptionResponseAssembler;
 
     @Mock
     private LoginService loginService;
-
-    @Mock
-    private HTTPExceptionResponseAssembler httpExceptionResponseAssembler;
 
     @Mock
     private LoginDto loginDto;
@@ -30,7 +35,21 @@ public class LoginResourceImplTest {
 
     @BeforeEach
     public void setUp() {
+        httpExceptionResponseAssembler = new HTTPExceptionResponseAssembler(new HTTPExceptionMapper());
         loginResource = new LoginResourceImpl(loginService, httpExceptionResponseAssembler, constraintsValidator);
+    }
+
+    @Test
+    public void givenInvalidConstraints_whenChooseBattery_thenReturnsAccordingErrorResponse(){
+
+        Mockito.doThrow(BadInputParameterException.class).when(constraintsValidator).validate(loginDto);
+
+        // when
+        Response response = loginResource.loginCustomer(loginDto);
+
+        // then
+        assertEquals(response.getStatus(), BAD_INPUT_PARAMETERS_ERROR_CODE);
+        assertEquals(response.getEntity(), BAD_INPUT_PARAMETERS_ERROR_MESSAGE);
     }
 
     @Test
--- src/test/java/ca/ulaval/glo4003/evulution/api/sale/SaleResourceImplTest.java
+++ src/test/java/ca/ulaval/glo4003/evulution/api/sale/SaleResourceImplTest.java
@@ -3,11 +3,14 @@ package ca.ulaval.glo4003.evulution.api.sale;
 import ca.ulaval.glo4003.evulution.api.assemblers.HTTPExceptionResponseAssembler;
 import ca.ulaval.glo4003.evulution.api.authorization.dto.TokenDto;
 import ca.ulaval.glo4003.evulution.api.authorization.dto.TokenDtoAssembler;
+import ca.ulaval.glo4003.evulution.api.exceptions.BadInputParameterException;
+import ca.ulaval.glo4003.evulution.api.mappers.HTTPExceptionMapper;
 import ca.ulaval.glo4003.evulution.api.sale.dto.ChooseBatteryDto;
 import ca.ulaval.glo4003.evulution.api.sale.dto.ChooseVehicleDto;
 import ca.ulaval.glo4003.evulution.api.validators.ConstraintsValidator;
 import ca.ulaval.glo4003.evulution.service.sale.SaleService;
 import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.core.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,15 +19,19 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @ExtendWith(MockitoExtension.class)
 public class SaleResourceImplTest {
     private final int A_TRANSACTION_ID = 2;
+    private static final Integer BAD_INPUT_PARAMETERS_ERROR_CODE = 400;
+    private static final String BAD_INPUT_PARAMETERS_ERROR_MESSAGE = "bad input parameter";
+
     private SaleResourceImpl saleResourceImpl;
 
     @Mock
     private SaleService saleService;
 
-    @Mock
     private HTTPExceptionResponseAssembler httpExceptionResponseAssembler;
 
     @Mock
@@ -47,8 +54,35 @@ public class SaleResourceImplTest {
 
     @BeforeEach
     private void setUp() {
+        httpExceptionResponseAssembler = new HTTPExceptionResponseAssembler(new HTTPExceptionMapper());
         saleResourceImpl = new SaleResourceImpl(saleService, tokenDtoAssembler, httpExceptionResponseAssembler,
                 constraintsValidator);
+    }
+
+    @Test
+    public void givenInvalidConstraints_whenChooseVehicle_thenReturnsAccordingErrorResponse(){
+
+        Mockito.doThrow(BadInputParameterException.class).when(constraintsValidator).validate(chooseVehicleDto);
+
+        // when
+        Response response = saleResourceImpl.chooseVehicle(A_TRANSACTION_ID, chooseVehicleDto);
+
+        // then
+        assertEquals(response.getStatus(), BAD_INPUT_PARAMETERS_ERROR_CODE);
+        assertEquals(response.getEntity(), BAD_INPUT_PARAMETERS_ERROR_MESSAGE);
+    }
+
+    @Test
+    public void givenInvalidConstraints_whenChooseBattery_thenReturnsAccordingErrorResponse(){
+
+        Mockito.doThrow(BadInputParameterException.class).when(constraintsValidator).validate(chooseBatteryDto);
+
+        // when
+        Response response = saleResourceImpl.chooseBattery(A_TRANSACTION_ID, chooseBatteryDto);
+
+        // then
+        assertEquals(response.getStatus(), BAD_INPUT_PARAMETERS_ERROR_CODE);
+        assertEquals(response.getEntity(), BAD_INPUT_PARAMETERS_ERROR_MESSAGE);
     }
 
     @Test
