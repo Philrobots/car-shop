b0770fa3a93761a2f658dc134d3ce5c9b9eee88b
fabrem <remilevesque17@gmail.com>
2021-10-12 09:53:13 -0400

    wip comment ca battery est private TBKKK

--- src/main/java/ca/ulaval/glo4003/evulution/api/assemblyline/VehicleAssemblyLineFacade.java
+++ src/main/java/ca/ulaval/glo4003/evulution/api/assemblyline/VehicleAssemblyLineFacade.java
@@ -1,39 +1,38 @@
 package ca.ulaval.glo4003.evulution.api.assemblyline;
 
+import ca.ulaval.glo4003.evulution.car_manufacture.BasicVehicleAssemblyLine;
 import ca.ulaval.glo4003.evulution.car_manufacture.BuildStatus;
-import ca.ulaval.glo4003.evulution.car_manufacture.BatteryAssemblyLine;
-import ca.ulaval.glo4003.evulution.car_manufacture.BasicBatteryAssemblyLine;
 import ca.ulaval.glo4003.evulution.car_manufacture.CommandID;
-import ca.ulaval.glo4003.evulution.domain.assemblyline.AssemblyLine;
 import ca.ulaval.glo4003.evulution.domain.assemblyline.AssemblyStatus;
+import ca.ulaval.glo4003.evulution.domain.assemblyline.BatteryAssemblyLine;
 import ca.ulaval.glo4003.evulution.domain.sale.TransactionId;
 
 import java.util.Map;
 
-public class BatteryAssemblyLineFacade implements AssemblyLine {
+public class VehicleAssemblyLineFacade implements BatteryAssemblyLine {
 
-    BatteryAssemblyLine batteryAssemblyLine = new BasicBatteryAsssemblyLine();
+    BasicVehicleAssemblyLine vehicleAssemblyLine = new BasicVehicleAssemblyLine();
 
     @Override
     public AssemblyStatus getStatus(TransactionId transactionId) {
         CommandID commandId = new CommandID(transactionId.toString());
-        BuildStatus status = batteryAssemblyLine.getBuildStatus(commandId);
+        BuildStatus status = vehicleAssemblyLine.getBuildStatus(commandId);
         return AssemblyStatus.valueOf(status.toString());
     }
 
     @Override
     public void newCommand(TransactionId transactionId, String command) {
         CommandID commandID = new CommandID(transactionId.toString());
-        batteryAssemblyLine.newBatteryCommand(commandID, command);
+        vehicleAssemblyLine.newCarCommand(commandID, command);
     }
 
     @Override
     public void configureAssemblyLine(Map<String, Integer> timeByCommand) {
-        batteryAssemblyLine.configureAssemblyLine(timeByCommand);
+        vehicleAssemblyLine.configureAssemblyLine(timeByCommand);
     }
 
     @Override
     public void advance() {
-        batteryAssemblyLine.advance();
+        vehicleAssemblyLine.advance();
     }
 }
