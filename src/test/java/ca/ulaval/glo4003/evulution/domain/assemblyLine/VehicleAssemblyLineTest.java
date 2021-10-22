cf0779f806ac42105bed1c9ab5925a52de51efd4
VinceBro <vbreault99@gmail.com>
2021-10-22 17:47:22 -0400

    tests

--- src/test/java/ca/ulaval/glo4003/evulution/domain/assemblyLine/VehicleAssemblyLineTest.java
+++ src/test/java/ca/ulaval/glo4003/evulution/domain/assemblyLine/VehicleAssemblyLineTest.java
@@ -3,7 +3,9 @@ package ca.ulaval.glo4003.evulution.domain.assemblyLine;
 import ca.ulaval.glo4003.evulution.domain.assemblyline.AssemblyStatus;
 import ca.ulaval.glo4003.evulution.domain.assemblyline.VehicleAssemblyAdapter;
 import ca.ulaval.glo4003.evulution.domain.assemblyline.VehicleAssemblyLine;
+import ca.ulaval.glo4003.evulution.domain.assemblyline.mediator.AssemblyLineMediator;
 import ca.ulaval.glo4003.evulution.domain.car.Car;
+import ca.ulaval.glo4003.evulution.domain.production.VehicleProduction;
 import ca.ulaval.glo4003.evulution.domain.sale.TransactionId;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,16 +13,15 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class VehicleAssemblyLineTest {
 
-    private static final int ZERO_SECONDS = 0;
     private static final int AN_INT = 1;
     private static final TransactionId A_TRANSACTION_ID = new TransactionId(AN_INT);
     private static final String A_CAR_NAME = "name";
+    private static final VehicleProduction vehicleProduction = new VehicleProduction(A_TRANSACTION_ID, A_CAR_NAME);
 
     private VehicleAssemblyLine vehicleAssemblyLine;
 
@@ -28,31 +29,63 @@ class VehicleAssemblyLineTest {
     private VehicleAssemblyAdapter vehicleAssemblyAdapter;
 
     @Mock
-    private Car car;
+    private AssemblyLineMediator assemblyLineMediator;
 
-    // @BeforeEach
-    // public void setup() {
-    // vehicleAssemblyLine = new VehicleAssemblyLine(vehicleAssemblyAdapter, ZERO_SECONDS);
-    // when(vehicleAssemblyAdapter.getStatus(A_TRANSACTION_ID)).thenReturn(AssemblyStatus.ASSEMBLED);
-    // when(car.getName()).thenReturn(A_CAR_NAME);
-    // }
-    //
-    // @Test
-    // public void whenCompleteVehicleCommand_thenShouldSetCarAsAssembled() {
-    // // when
-    // vehicleAssemblyLine.completeVehicleCommand(A_TRANSACTION_ID, car);
-    //
-    // // then
-    // verify(car).setCarAsAssembled();
-    // }
-    //
-    // @Test
-    // public void whenCompleteVehicleCommand_thenCallsNewBatteryCommand() {
-    // // when
-    // vehicleAssemblyLine.completeVehicleCommand(A_TRANSACTION_ID, car);
-    //
-    // // then
-    // verify(vehicleAssemblyAdapter).newVehicleCommand(A_TRANSACTION_ID, A_CAR_NAME);
-    // }
+    @BeforeEach
+    public void setup() {
+        vehicleAssemblyLine = new VehicleAssemblyLine(vehicleAssemblyAdapter);
+        vehicleAssemblyLine.setMediator(assemblyLineMediator);
+    }
 
+    @Test
+    public void givenMultipleProductions_whenAddProduction_thenShouldCreateVehicleCommandOnce() {
+        // when
+        vehicleAssemblyLine.addProduction(vehicleProduction);
+        vehicleAssemblyLine.addProduction(vehicleProduction);
+
+        // then
+        verify(vehicleAssemblyAdapter, times(1)).newVehicleCommand(A_TRANSACTION_ID, A_CAR_NAME);
+    }
+
+    @Test
+    public void givenNoProduction_whenAdvance_thenDoesNothing() {
+        // when
+        vehicleAssemblyLine.advance();
+
+        // then
+        verify(vehicleAssemblyAdapter, times(0)).newVehicleCommand(A_TRANSACTION_ID, A_CAR_NAME);
+        verify(vehicleAssemblyAdapter, times(0)).getStatus(A_TRANSACTION_ID);
+        verify(assemblyLineMediator, times(0)).notify(VehicleAssemblyLine.class);
+    }
+
+    @Test
+    public void givenOneProduction_whenAdvance_thenGetsStatusAndNotifies() {
+        // given
+        vehicleAssemblyLine.addProduction(vehicleProduction);
+        when(vehicleAssemblyAdapter.getStatus(A_TRANSACTION_ID)).thenReturn(AssemblyStatus.ASSEMBLED);
+
+        // when
+        vehicleAssemblyLine.advance();
+
+        // then
+        verify(vehicleAssemblyAdapter, times(1)).newVehicleCommand(A_TRANSACTION_ID, A_CAR_NAME);
+        verify(vehicleAssemblyAdapter, times(1)).getStatus(A_TRANSACTION_ID);
+        verify(assemblyLineMediator, times(1)).notify(VehicleAssemblyLine.class);
+    }
+
+    @Test
+    public void givenTwoProductions_whenAdvance_thenGetsStatusNotifiesAndAddsCommand() {
+        // given
+        vehicleAssemblyLine.addProduction(vehicleProduction);
+        vehicleAssemblyLine.addProduction(vehicleProduction);
+        when(vehicleAssemblyAdapter.getStatus(A_TRANSACTION_ID)).thenReturn(AssemblyStatus.ASSEMBLED);
+
+        // when
+        vehicleAssemblyLine.advance();
+
+        // then
+        verify(vehicleAssemblyAdapter, times(2)).newVehicleCommand(A_TRANSACTION_ID, A_CAR_NAME);
+        verify(vehicleAssemblyAdapter, times(1)).getStatus(A_TRANSACTION_ID);
+        verify(assemblyLineMediator, times(1)).notify(VehicleAssemblyLine.class);
+    }
 }
